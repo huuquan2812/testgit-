4861cecc813b88a58a571fbbad9a2e1acc3fc2a6
huuquan2812 <huuquan281297@gmail>
2020-10-10 16:25:54 +0700

    add wordpress.sh

---
+++ wordpress.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+#cai dat apache2 
+sudo apt update 
+sudo apt install apache2 
+sudo systemctl status 
+
+#cai dat mysql 
+sudo apt update 
+sudo apt install mysql-server
+#cai dat php7 
+sudo apt update
+sudo apt updatesudo apt install php libapache2-mod-php
+sudo apt install php-curl php-gd php-mbstring php-xml php-xmlrpc php-soap php-intl php-zip
+#cai dat wordpress 
+
