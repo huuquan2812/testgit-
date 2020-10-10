ff44ca94859b0ef87a0358eeb66a9960439f94be
huuquan2812 <huuquan281297@gmail>
2020-10-10 16:51:07 +0700

    update wordpress.sh

--- wordpress.sh
+++ wordpress.sh
@@ -1,4 +1,5 @@
-#!/bin/bash 
+#!/bin/bash
+set -e
 #cai dat apache2 
 sudo apt update 
 sudo apt install -y apache2 
@@ -8,7 +9,7 @@ sudo systemctl status
  
 sudo apt install -y mysql-server
 #tao database wordpress 
-echo "create database quan.test.wordpress" | mysql -u root -p"Luonghuuquan1!"
+echo "create database quan_test_wordpress" | mysql -u root -p"Luonghuuquan1!"
 #cai dat php7 
 sudo  apt install -y php libapache2-mod-php
 sudo apt install -y  php-curl php-gd php-mbstring php-xml php-xmlrpc php-soap php-intl php-zip
