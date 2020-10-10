dbc1220d80c71551a83282e20314070c90a7e453
huuquan2812 <huuquan281297@gmail>
2020-10-10 16:36:08 +0700

    wordpress

--- wordpress.sh
+++ wordpress.sh
@@ -1,14 +1,14 @@
 #!/bin/bash 
 #cai dat apache2 
 sudo apt update 
-sudo apt install apache2 
+sudo apt install -y apache2 
 sudo systemctl status 
 
 #cai dat mysql 
  
-sudo apt install mysql-server
+sudo apt install -y mysql-server
 #cai dat php7 
-sudo apt updatesudo apt install php libapache2-mod-php
-sudo apt install php-curl php-gd php-mbstring php-xml php-xmlrpc php-soap php-intl php-zip
+sudo  apt install -y php libapache2-mod-php
+sudo apt install -y  php-curl php-gd php-mbstring php-xml php-xmlrpc php-soap php-intl php-zip
 #cai dat wordpress 
 
