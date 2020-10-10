0716e8aea37006bac45230949b4978b8704f39fd
huuquan2812 <huuquan281297@gmail>
2020-10-10 16:30:45 +0700

    wordpress.sh

--- wordpress.sh
+++ wordpress.sh
@@ -5,10 +5,9 @@ sudo apt install apache2
 sudo systemctl status 
 
 #cai dat mysql 
-sudo apt update 
+ 
 sudo apt install mysql-server
 #cai dat php7 
-sudo apt update
 sudo apt updatesudo apt install php libapache2-mod-php
 sudo apt install php-curl php-gd php-mbstring php-xml php-xmlrpc php-soap php-intl php-zip
 #cai dat wordpress 
